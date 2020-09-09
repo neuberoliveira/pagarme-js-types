2b002e3724fefe2556bdf8b389b9a309352f6233
Neuber Oliveira <neuber.oliveira05@gmail.com>
2020-09-09 12:56:11 -0300

    fix: remove extends Address from Documents official docs: https://docs.pagar.me/reference#objeto-documents-documentos

--- index.d.ts
+++ index.d.ts
@@ -479,7 +479,7 @@ declare module 'pagarme' {
 
   export type DocumentType = 'individual' | 'corporation' | 'other';
 
-  export interface Document extends Address {
+  export interface Document {
     /** Tipo de documento. Para compradores brasileiros, deve ser fornecido ao menos um CPF (no caso de pessoa física, i.e. `individual`) ou CNPJ (no caso de pessoa jurídica, i.e. `corporation`). Para compradores internacionais, o documento pode ser um passaporte (type `passport`) ou um campo personalizado (type `other`). */
     type: DocumentType;
     /** Número do documento */
